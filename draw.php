92ed29b4a3e8a1bb4ef9a5798278eaa5ffc7571e
Igor Polyakov <fox.user.3@gmail.com>
2014-11-10 14:44:40 +0600

    I LOVE DO COMMIT

--- draw.php
+++ draw.php
@@ -24,23 +24,23 @@ foreach ($db->query('SELECT * FROM inform WHERE Id > (SELECT MAX(Id)-'.$last_dat
 }
 
 
-	$MyData = new pData();
-	// $MyData->addPoints(array(-4,VOID,VOID,12,8,3),"Probe 1");
-	// $MyData->addPoints(array(3,12,15,8,5,-5),"Probe 2");
-	$MyData->addPoints($points_tempr,"Temperatures");
-	// $MyData->setSerieTicks("Probe 2",4);
-	$MyData->setSerieWeight("Temperatures",1);
-	//$MyData->setAxisName(0,"Temperatures");
-	$MyData->addPoints($points_date,"Labels");
-	$MyData->setSerieDescription("Labels","Months");
-	$MyData->setAbscissa("Labels");
+$MyData = new pData();
+// $MyData->addPoints(array(-4,VOID,VOID,12,8,3),"Probe 1");
+// $MyData->addPoints(array(3,12,15,8,5,-5),"Probe 2");
+$MyData->addPoints($points_tempr,$query_png);
+// $MyData->setSerieTicks("Probe 2",4);
+$MyData->setSerieWeight("Temperatures",1);
+//$MyData->setAxisName(0,"Temperatures");
+$MyData->addPoints($points_date,"Labels");
+$MyData->setSerieDescription("Labels","Months");
+$MyData->setAbscissa("Labels");
 
 $w = isset($_GET['w']) ? $_GET['w'] : 1250;
 $h = isset($_GET['h']) ? $_GET['h'] : 550;
 
  /* Create the pChart object */
  $myPicture = new pImage($w,$h,$MyData);
-$Settings = array("R"=>32, "G"=>32, "B"=>32, "Dash"=>1, "DashR"=>52, "DashG"=>52, "DashB"=>52);
+$Settings = array("R"=>32, "G"=>32, "B"=>32, "Dash"=>1, "DashR"=>22, "DashG"=>22, "DashB"=>22);
 $myPicture->drawFilledRectangle(0,0,$w,$h,$Settings);
 
 $Settings = array("StartR"=>219, "StartG"=>231, "StartB"=>139, "EndR"=>1, "EndG"=>138, "EndB"=>68, "Alpha"=>0);
@@ -57,6 +57,11 @@ $Settings = array("Pos"=>SCALE_POS_LEFTRIGHT
 , "LabelingMethod"=>LABELING_ALL
 , "GridR"=>255, "GridG"=>255, "GridB"=>255, "GridAlpha"=>100, "TickR"=>0, "TickG"=>0, "TickB"=>0, "TickAlpha"=>50, "LabelRotation"=>0, "LabelSkip"=>$last_data/5, "CycleBackground"=>1, "DrawXLines"=>1, "DrawSubTicks"=>1, "SubTickR"=>0, "SubTickG"=>0, "SubTickB"=>0, "SubTickAlpha"=>50, "DrawYLines"=>ALL);
 $myPicture->drawScale($Settings);
+
+$Config = array("FontR"=>0, "FontG"=>0, "FontB"=>0, "FontName"=>"fonts/pf_arma_five.ttf", "FontSize"=>6, "Margin"=>6, "Alpha"=>30, "BoxSize"=>5, "Style"=>LEGEND_BOX
+, "Mode"=>LEGEND_HORIZONTAL
+);
+$myPicture->drawLegend($border*2,$border*1.5,$Config);
 
 $Config = array("DisplayValues"=>1, "ForceTransparency"=>50, "AroundZero"=>1);
 $myPicture->$typeGraph($Config);
--- version.php
+++ version.php
@@ -1,2 +1,2 @@
-<?php echo 'Version 0.4 at Mon, 10 Nov 2014 13:29:11 +0600
+<?php echo 'Version 0.5 at Mon, 10 Nov 2014 14:07:06 +0600
 ';
