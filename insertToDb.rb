954e3eb5a15f5bd9265252b53c82abde7ce5a6fa
IgorPolyakov <igorpolyakov@protonmail.com>
2017-10-13 23:47:40 +0700

    clean rb code

--- insertToDb.rb
+++ insertToDb.rb
@@ -1,58 +1,54 @@
 #!/usr/bin/ruby
-# temperature   pressure    humidity    light 
+# temperature   pressure    humidity    light
 require 'mysql'
 require 'time'
 require 'date'
-require "serialport"
+require 'serialport'
 def insert(con, data)
-	    con.query("CREATE TABLE IF NOT EXISTS inform(Id INT PRIMARY KEY AUTO_INCREMENT, temperature  INT(3), pressure INT(3), humidity INT(4), light INT(1),time TIMESTAMP)")
-        p "start write"
-    #100.times do
-    	str = "INSERT INTO inform(temperature, pressure, humidity, light, time) VALUES('#{data[1].to_s}', '#{data[0].to_s}', '#{rand(720 .. 800).to_s}','#{rand(0 .. 1).to_s}',CURRENT_TIMESTAMP())"
-        p "Sucsesfull :: " + str
-        con.query(str)
-        sleep(10)
-    #end
+  con.query('CREATE TABLE IF NOT EXISTS inform(Id INT PRIMARY KEY AUTO_INCREMENT, temperature  INT(3), pressure INT(3), humidity INT(4), light INT(1),time TIMESTAMP)')
+  puts 'start write'
+  # 100.times do
+  str = "INSERT INTO inform(temperature, pressure, humidity, light, time) VALUES('#{data[1]}', '#{data[0]}', '#{rand(720..800)}','#{rand(0..1)}',CURRENT_TIMESTAMP())"
+  puts 'Sucsesfull :: ' + str
+  con.query(str)
+  sleep(10)
+  # end
 end
 
 def delete(con)
-	con.query "DROP TABLE inform"
-	p "Sucsesfull delete all"
+  con.query 'DROP TABLE inform'
+  puts 'Sucsesfull delete all'
 end
 
 def select(con)
-	p con.query "SELECT `light` FROM `inform` WHERE `Id` = (SELECT MAX( `Id` )FROM `inform` )"
-	p "Sucsesfull select"
+  puts con.query 'SELECT `light` FROM `inform` WHERE `Id` = (SELECT MAX( `Id` )FROM `inform` )'
+  puts 'Sucsesfull select'
 end
 
 # https://github.com/hparra/ruby-serialport
- 
- 
-#params for serial port
-port_str = "/dev/ttyUSB0"  #may be different for you
+
+# params for serial port
+port_str = '/dev/ttyUSB0' # may be different for you
 baud_rate = 9600
 data_bits = 8
 stop_bits = 1
-data = ""
+data = ''
 
-p "strart + #{DateTime.now}"
+puts "strart + #{DateTime.now}"
 con = Mysql.new 'localhost', 'root', 'mys1234!@#$', 'data'
-p "connect"
-p "insert"
+puts 'connect'
+puts 'insert'
 
 parity = SerialPort::NONE
 
 sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
-while true do
-	message = sp.gets
-	if message
-		message.chomp
-		data = message.split(';')
-		#p data[0]
-		insert(con, data)
-		
-
-	end
+loop do
+  message = sp.gets
+  next unless message
+  message.chomp
+  data = message.split(';')
+  # puts data[0]
+  insert(con, data)
 end
 
 sp.close
